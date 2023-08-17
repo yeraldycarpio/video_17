8c9bc6840ea4053cf849ce5f39b17deac87de035
yeraldycarpio <yeraldycarpio_morales@hotmail.com>
2023-08-17 09:42:31 -0600

    Agregar archivos de proyecto.

---
+++ video_17/video_17/Alumno.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace video_17
+{
+	public class Alumno : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		string nombre = string.Empty;
+
+		public string Nombre
+		{
+			get => nombre;
+			set
+			{
+				if (nombre == value)
+					return;
+				nombre = value;
+				onPropertyChaged(nameof(Nombre));
+				onPropertyChaged(nameof(MostrarNombre));
+			}
+				}
+
+		public string MostrarNombre =>$"Nombre ingresado:{Nombre}";
+
+		void onPropertyChaged(string nombre)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nombre));
+
+		}
+	}
+}
